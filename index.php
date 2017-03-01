d5ceaf57247ba0bb261c58be318750e9b33b6826
Michael Roitzsch <reactorcontrol@icloud.com>
2017-03-01 11:42:59 +0100

    basic error handling

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-	switch ($_SERVER['QUERY_STRING']) {
-	default:
+function fatalError($code) {
+	switch ($code) {
+	case 404:
 		header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+		break;
+	default:
+	case 500:
+		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+		break;
+	case 501:
+		header($_SERVER['SERVER_PROTOCOL'] . ' 501 Not Implemented');
+		break;
 	}
+	exit();
+}
+
+/* dispatch query string commands to their implementations */
+switch ($_SERVER['QUERY_STRING']) {
+default:
+	fatalError(501);
+}
