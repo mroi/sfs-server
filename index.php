4173ffffa5b580e1bf3a07a11d4d4366e3a70fdf
Michael Roitzsch <reactorcontrol@icloud.com>
2017-03-03 12:41:17 +0100

    human readable error output
    • function for HTML output with configurable body and header
    • localization using language preference reported by the browser

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 require('command.php');
 
+function html(string $body, $header = '') {
+	$title = l10n("File Sharing");
+	// TODO: header += bootstrap loading
+	print("<!DOCTYPE html><html><head><title>${title}</title>${header}</head><body>${body}</body></html>");
+}
+
 abstract class Assertion {
 	const Root = 0;
 }
@@ -21,19 +27,24 @@ function fatalError($code = 500) {
 	switch ($code) {
 	case 400:
 		$header = '400 Bad Request';
+		$message = 'The request is malformed.';
 		break;
 	case 404:
 		$header = '404 Not Found';
+		$message = 'The file does not exist.';
 		break;
 	default:
 	case 500:
 		$header = '500 Internal Server Error';
+		$message = 'An internal error occurred.';
 		break;
 	case 501:
 		$header = '501 Not Implemented';
+		$message = 'The command is not implemented.';
 		break;
 	}
 	header($_SERVER['SERVER_PROTOCOL'] . ' ' . $header);
+	html('<h1>' . l10n($message) . '</h1>');
 	exit();
 }
 
