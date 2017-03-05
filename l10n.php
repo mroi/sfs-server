a388dde719788845ccb0690d5e6cd549a4f37316
Michael Roitzsch <reactorcontrol@icloud.com>
2017-03-05 19:25:16 +0100

    German localization

--- l10n.php
+++ l10n.php
@@ -1,4 +1,31 @@
 <?php
 
 $translations = array(
+	'An unknown error occurred.' => array(
+		'de' => 'Ein unbekannter Fehler ist aufgetreten.'
+	),
+	'File Not Found' => array(
+		'de' => 'Datei nicht gefunden'
+	),
+	'File Sharing' => array(
+		'de' => 'Dateifreigabe'
+	),
+	'Internal Error' => array(
+		'de' => 'Interner Fehler'
+	),
+	'Malformed Request' => array(
+		'de' => 'Ungültige Anfrage'
+	),
+	'The command you sent is not implemented.' => array(
+		'de' => 'Das gesendete Kommande ist nicht implementiert.'
+	),
+	'The requested file does not exist or it has been deleted.' => array(
+		'de' => 'Die angeforderte Datei existiert nicht oder wurde gelöscht.'
+	),
+	'Unknown Command' => array(
+		'de' => 'Unbekanntes Kommando'
+	),
+	'You have sent an invalid request that cannot work.' => array(
+		'de' => 'Du hast eine unerlaubte Anfrage gesendet, die nicht funktionieren kann.'
+	)
 );
